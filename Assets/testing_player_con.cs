5731ebe0a1018496dd3fd04aaf271d0003347d07
hijomo <hijomo@gmail.com>
2016-11-11 17:41:13 -0700

    setting up warp

---
+++ Assets/testing_player_con.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testing_player_con : MonoBehaviour {
+
+  float speed = 20f;
+  Transform tra;
+	// Use this for initialization
+	void Start () {
+    tra = this.GetComponent<Transform>();
+    
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+    float deltaY = Input.GetAxis("Vertical");
+    float deltaX = Input.GetAxis("Horizontal");
+    Vector3 move = new Vector3(deltaX * speed* Time.deltaTime, deltaY * speed* Time.deltaTime, 0);
+    tra.Translate(move, Space.World);
+    
+	}
+}
