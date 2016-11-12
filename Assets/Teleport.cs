af76238caa2f2b71e866fc3be0124febd551abe0
hijomo <hijomo@gmail.com>
2016-11-12 11:57:00 -0700

    added a hat shop

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -4,31 +4,55 @@ using System.Collections;
 public class Teleport : MonoBehaviour {
 
   GameObject player;
-  GameObject exteriorDoor;
-  GameObject interiorDoor;
+  GameObject saloonExteriorDoor;
+  GameObject saloonInteriorDoor;
+  GameObject hatShopExteriorDoor;
+  GameObject hatShopInteriorDoor;
+  float doorOffset = 1.0f;
 	// Use this for initialization
 	void Start () {
     player = GameObject.FindGameObjectWithTag("Player");
-    exteriorDoor = GameObject.FindGameObjectWithTag("SaloonDoorExterior");
-    interiorDoor = GameObject.FindGameObjectWithTag("SaloonDoorInterior");
+    saloonExteriorDoor = GameObject.FindGameObjectWithTag("SaloonDoorExterior");
+    saloonInteriorDoor = GameObject.FindGameObjectWithTag("SaloonDoorInterior");
+    hatShopExteriorDoor = GameObject.FindGameObjectWithTag("HatShopDoorExterior");
+    hatShopInteriorDoor = GameObject.FindGameObjectWithTag("HatShopDoorInterior");
 	}
   
 
   void OnTriggerEnter2D(Collider2D other)
   {
- 
-    
-    if (other.gameObject == exteriorDoor)
+    // when the player enters the collider for a door they will be translated to the coresponding door 
+
+    //Saloon doors
+    if (other.gameObject == saloonExteriorDoor)
     {
-      Vector3 reposition = interiorDoor.GetComponent<Transform>().position;
-      reposition.y += 2;
+      Vector3 reposition = saloonInteriorDoor.GetComponent<Transform>().position;
+      reposition.y += doorOffset;
       reposition.z = -5;
       player.GetComponent<Transform>().position = reposition;
     }
-    if (other.gameObject == interiorDoor)
+
+    if (other.gameObject == saloonInteriorDoor)
     {
-      Vector3 reposition = exteriorDoor.GetComponent<Transform>().position;
-      reposition.y -= 2;
+      Vector3 reposition = saloonExteriorDoor.GetComponent<Transform>().position;
+      reposition.y -= doorOffset;
+      reposition.z = -5;
+      player.GetComponent<Transform>().position = reposition;
+    }
+
+    // Hat Shop Doors
+    if (other.gameObject == hatShopExteriorDoor)
+    {
+      Vector3 reposition = hatShopInteriorDoor.GetComponent<Transform>().position;
+      reposition.y += doorOffset;
+      reposition.z = -5;
+      player.GetComponent<Transform>().position = reposition;
+    }
+
+    if (other.gameObject == hatShopInteriorDoor)
+    {
+      Vector3 reposition = hatShopExteriorDoor.GetComponent<Transform>().position;
+      reposition.y -= doorOffset;
       reposition.z = -5;
       player.GetComponent<Transform>().position = reposition;
     }
