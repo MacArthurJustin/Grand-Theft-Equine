4391e8284913b20967440c94b75d916915215ef7
hijomo <hijomo@gmail.com>
2016-11-12 21:41:23 -0700

    added a canvas

--- Assets/NPCCharactor.cs
+++ Assets/NPCCharactor.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NPCCharactor : Character {
+public class NPCCharactor : Character, IInteractable
+{
 
-	// Use this for initialization
-	void Start () {
-	
+  private float repdiff = 0;
+ public bool CanInteract { get { return Mathf.Abs (repdiff) < 10; } }
+ public void Interact(PlayableCharacter PC) {
+  //  repdiff = 
+  }
+  
+
+  // Use this for initialization
+  void Start () {
+    
 	}
 	
 	// Update is called once per frame
