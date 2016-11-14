93e4a01085180a6addee4cdc1fc3978618771df4
hijomo <hijomo@gmail.com>
2016-11-13 17:57:19 -0700

    map done, needs testing

--- Assets/WorldWrap.cs
+++ Assets/WorldWrap.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class WorldWrap : MonoBehaviour {
 
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject == GameObject.FindGameObjectWithTag("Player"))
+    {
+      other.transform.position = Vector3.zero;
+    }
+  }
 	// Use this for initialization
 	void Start () {
 	
